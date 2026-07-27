0a49a43384001308bbaf8712515ac822c06d3938
Thomas Fredhall Holguin <thomasfredhallholguin@gmail.com>
2026-07-27 22:05:28 +0200

    update field

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : NetworkBehaviour
     private Vector3 velocity;
     
     [SerializeField] private Transform cameraPivot; // Assign your Camera or a pivot object
-    [SerializeField] private float lookSpeed = 120f;
+    [SerializeField] private float lookSpeed = 300f;
     [SerializeField] private float minLookAngle = -80f;
     [SerializeField] private float maxLookAngle = 80f;
 
